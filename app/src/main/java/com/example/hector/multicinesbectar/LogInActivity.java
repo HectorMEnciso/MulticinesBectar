ab4ace883b712befa1e8c2aea24558bc5b314e56
HectorMEnciso <hectorencisolezcano@gmail.com>
2015-04-06 21:29:55 +0200

    Interface

--- app/src/main/java/com/example/hector/multicinesbectar/LogInActivity.java
+++ app/src/main/java/com/example/hector/multicinesbectar/LogInActivity.java
@@ -2,15 +2,45 @@ package com.example.hector.multicinesbectar;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Hector on 29/03/2015.
  */
 public class LogInActivity extends Activity {
-
+    private boolean estaMostrada=false;
+    private EditText EditPassword;
+    private Button ShowPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        EditPassword=(EditText)findViewById(R.id.txtPassword);
+        ShowPassword=(Button)findViewById(R.id.ShowPassword);
+
+        ShowPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(estaMostrada){
+                    EditPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+                estaMostrada=false;
+            }
+        });
+
+        ShowPassword.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                estaMostrada=true;
+                EditPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                return estaMostrada;
+            }
+        });
     }
 }
