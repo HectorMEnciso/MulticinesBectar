1b28a891f1003bb682c69f2425984f0b2c4549b7
HectorMEnciso <hectorencisolezcano@gmail.com>
2015-04-06 21:34:32 +0200

    Interface

--- app/src/main/java/com/example/hector/multicinesbectar/LogInActivity.java
+++ app/src/main/java/com/example/hector/multicinesbectar/LogInActivity.java
@@ -1,6 +1,7 @@
 package com.example.hector.multicinesbectar;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by Hector on 29/03/2015.
@@ -15,6 +17,7 @@ import android.widget.EditText;
 public class LogInActivity extends Activity {
     private boolean estaMostrada=false;
     private EditText EditPassword;
+    private TextView link_to_register;
     private Button ShowPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,15 @@ public class LogInActivity extends Activity {
 
         EditPassword=(EditText)findViewById(R.id.txtPassword);
         ShowPassword=(Button)findViewById(R.id.ShowPassword);
+        link_to_register=(TextView)findViewById(R.id.link_to_register);
+
+        link_to_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent data = new Intent(getApplicationContext(),SignInActivity.class);
+                startActivity(data);
+            }
+        });
 
         ShowPassword.setOnClickListener(new View.OnClickListener() {
             @Override
